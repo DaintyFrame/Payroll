988a30a6b5a093f0f2eb820c70f39786317bea1b
DaintyFrame <Josephin.Esc@mail.com>
2021-04-06 21:17:47 +0800

    新建testAddHourlyEmployeeTransaction类 (添加小时工)

--- src/payroll/test/AddEmployeeTest.java
+++ src/payroll/test/AddEmployeeTest.java
@@ -4,11 +4,29 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+
+/*
+      用例1：增加雇员。
+使用AddEmp操作(transaction)增加新雇员。该操作包含有分配的雇员号、雇员的名字及其
+地址。该操作有3种形式：
+AddEmp EmpId “name” “address” H hourly-rate
+AddEmp EmpId “name" “address” S monthly-salary
+AddEmp EmpId “name” “address” C monthly-salary commission-rate
+雇员记录是根据对应字段的值来创建的。
+异常情况1：描述操作的结构有错误。
+如果描述操作的结构不正确，会打印一条错误消息，不进行任何处理。
+ */
 public class AddEmployeeTest {
 
+//	@Test
+//	void test() {
+//		fail("Not yet implemented");
+//	}
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testAddHourlyEmployeeTransaction() {
+		
+		
 	}
+	
 
 }
