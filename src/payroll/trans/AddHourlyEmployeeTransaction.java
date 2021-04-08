5167a0145895885f9072620bf20ca3c55509f389
DaintyFrame <Josephin.Esc@mail.com>
2021-04-08 20:23:57 +0800

    添加HourlyClassification方法

--- src/payroll/trans/AddHourlyEmployeeTransaction.java
+++ src/payroll/trans/AddHourlyEmployeeTransaction.java
@@ -1,6 +1,8 @@
 package payroll.trans;
 
+import payroll.Employee;
 import payroll.Transaction;
+import payroll.classifcation.HourlyClassification;
 
 public class AddHourlyEmployeeTransaction implements Transaction {
 
@@ -19,7 +21,12 @@ public class AddHourlyEmployeeTransaction implements Transaction {
 	@Override
 	public void execute() {
 		// TODO Auto-generated method stub
-		
+		//新建雇员
+		Employee employee = new Employee(empId,name,address);
+		//设置工资计算方式
+		employee.setPaymentClassification(new HourlyClassification(hourlyRate));
+		//设置工资支付方法
+		//保存到数据库
 	}
 
 }
