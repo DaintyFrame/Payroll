f71095093923079656343537c2625cd17d8be7ef
DaintyFrame <Josephin.Esc@mail.com>
2021-04-09 10:48:16 +0800

    添加代码，并生成DeleteEmployeeTransaction

--- src/payroll/test/DeleteEmployeeTest.java
+++ src/payroll/test/DeleteEmployeeTest.java
@@ -3,6 +3,10 @@ package payroll.test;
 
 import org.junit.jupiter.api.Test;
 
+import payroll.Transaction;
+import payroll.trans.AddHourlyEmployeeTransaction;
+import payroll.trans.DeleteEmployeeTransaction;
+
 class DeleteEmployeeTest {
 
 	@Test
@@ -10,5 +14,14 @@ class DeleteEmployeeTest {
 //		fail("Not yet implemented");
 //	}
 	public void testDeleteEmployeeExists() {
+		//≤Â»ÎπÕ‘±
+		int empId = 1623111006;
+		new AddHourlyEmployeeTransaction(empId, "zxb-006", "BZUSOFT", 2048.0).execute();
+
+		Transaction t = new DeleteEmployeeTransaction(empId);
+		t.execute();
+
+		Employee e = PayrollDatabase.getEmployee(empId);
+		assertNull(e);
 	}
 }
