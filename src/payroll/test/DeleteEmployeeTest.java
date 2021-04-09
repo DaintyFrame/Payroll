1df1929a9d08433f0ca220a9ae180fe392073e6f
DaintyFrame <Josephin.Esc@mail.com>
2021-04-09 10:45:58 +0800

    新建测试并编写测试

---
+++ src/payroll/test/DeleteEmployeeTest.java
@@ -0,0 +1,14 @@
+package payroll.test;
+
+
+import org.junit.jupiter.api.Test;
+
+class DeleteEmployeeTest {
+
+	@Test
+//	void test() {
+//		fail("Not yet implemented");
+//	}
+	public void testDeleteEmployeeExists() {
+	}
+}
