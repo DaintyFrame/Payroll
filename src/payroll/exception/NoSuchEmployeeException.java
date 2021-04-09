6f064fb9497806df7300f8ecdc2ecd580e433441
DaintyFrame <Josephin.Esc@mail.com>
2021-04-09 11:07:33 +0800

    构造方法

--- src/payroll/exception/NoSuchEmployeeException.java
+++ src/payroll/exception/NoSuchEmployeeException.java
@@ -2,6 +2,13 @@ package payroll.exception;
 
 public class NoSuchEmployeeException extends RuntimeException {
 
+	private String string;
+
+	public NoSuchEmployeeException(String string) {
+		this.string = string;
+		// TODO Auto-generated constructor stub
+	}
+
 	/**
 	 * 
 	 */
