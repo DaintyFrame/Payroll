b80637f2dc92c902a74e55994b231beeddf2c2c9
DaintyFrame <Josephin.Esc@mail.com>
2021-04-09 10:48:46 +0800

    DeleteEmployeeTransaction构造方法

--- src/payroll/trans/DeleteEmployeeTransaction.java
+++ src/payroll/trans/DeleteEmployeeTransaction.java
@@ -4,6 +4,13 @@ import payroll.Transaction;
 
 public class DeleteEmployeeTransaction implements Transaction {
 
+	private int empId;
+
+	public DeleteEmployeeTransaction(int empId) {
+		this.empId = empId;
+		// TODO Auto-generated constructor stub
+	}
+
 	@Override
 	public void execute() {
 		// TODO Auto-generated method stub
