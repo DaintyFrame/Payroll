af4999222cfcb2bccbdd890e1ea7d16f60fb6bcb
DaintyFrame <Josephin.Esc@mail.com>
2021-04-09 11:17:03 +0800

    缩进

--- src/payroll/exception/NoSuchEmployeeException.java
+++ src/payroll/exception/NoSuchEmployeeException.java
@@ -1,16 +1,10 @@
 package payroll.exception;
 
 public class NoSuchEmployeeException extends RuntimeException {
-
+private static final long serialVersionUID = 1L;
 
 	public NoSuchEmployeeException(String string) {
 		super(string);
 		// TODO Auto-generated constructor stub
 	}
-
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-
 }
